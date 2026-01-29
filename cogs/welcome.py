140fead53976edc0a974da0f161e8a1d561960c2
Ryuga <vaishnav2353@gmail.com>
2026-01-29 20:17:36 +0530

    Remove logging and DM

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -35,19 +35,6 @@ class Welcome(commands.Cog):
             return
 
         channel = guild.get_channel(constants.general_channel_id) or guild.system_channel
-        log_channel = guild.get_channel(constants.system_log_channel_id)
-
-        try:
-            await log_channel.send(
-                content=(
-                    f"👋 **Member Joined**\n"
-                    f"User: **{member.name}** (`{member.id}`)\n"
-                    f"Account created: <t:{int(member.created_at.timestamp())}:R>"
-                )
-            )
-        except discord.Forbidden:
-            pass
-
         try:
             await member.add_roles(welcome_role, reason="Welcome role added")
             await self.manager.schedule_removal(
@@ -56,30 +43,6 @@ class Welcome(commands.Cog):
                 role_id=welcome_role.id,
                 days=WELCOME_ROLE_DURATION_DAYS,
             )
-        except discord.Forbidden:
-            pass
-
-        try:
-            content_footer = (
-                f"Links: [Website]({constants.website_url}) | "
-                f"[Rules]({constants.rules_url}) | "
-                f"[Events]({constants.events_url})"
-            )
-            dm_embed = discord.Embed(
-                title=f"Welcome to {guild.name}!",
-                description=(
-                    f"Introduce yourself in <#{constants.general_channel_id}>\n\n"
-                    f"Leetcode discussion <#{constants.leetcode_channel_id}>\n\n"
-                    f"For **Leetcode challenges** checkout <#{constants.challenges_channel_id}>\n\n\n"
-                    + content_footer
-                ),
-                color=discord.Color.green(),
-            )
-            dm_embed.set_footer(
-                text="Tortoise Programming Community",
-                icon_url="https://lairesit.sirv.com/Images/tortoise.png",
-            )
-            await member.send(embed=dm_embed)
         except discord.Forbidden:
             pass
 
@@ -102,20 +65,6 @@ class Welcome(commands.Cog):
 
         self.leaves_today += 1
 
-        channel = guild.get_channel(constants.system_log_channel_id)
-        if channel is None:
-            return
-        try:
-            await channel.send(
-                content=(
-                    f"👋 **Member Left**\n"
-                    f"User: **{member.name}** (`{member.id}`)\n"
-                    f"Joined server: <t:{int(member.joined_at.timestamp())}:R>\n"
-                    f"Account created: <t:{int(member.created_at.timestamp())}:R>"
-                )
-            )
-        except discord.Forbidden:
-            pass
 
     @tasks.loop(time=dtime(hour=0, minute=0, tzinfo=timezone.utc))
     async def daily_retention_report(self):
